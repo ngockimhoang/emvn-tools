1df20d6c4894e6fb4a1c9e8297d96773df96b484
Kim Hoang <kimyumiko@gmail.com>
2020-06-08 00:02:20 +0700

    no message

--- TakeDownAsset/Program.cs
+++ TakeDownAsset/Program.cs
@@ -17,6 +17,7 @@ namespace TakeDownAsset
             var youtubeReportPath = @"D:\GoProjects\src\emvn-minions\youtube-asset-cli\input\asset_full_report_Audiomachine_L_v1-1.csv";
             var referencePath = @"C:\Users\kimhoang\Desktop\EMVN\take_down_reference.csv";
             var ownershipPath = @"C:\Users\kimhoang\Desktop\EMVN\take_down_ownership.csv";
+            //var atPath = @"C:\Users\kimhoang\Desktop\EMVN\take_down_art_track.csv";
             var assetList = new Dictionary<string, YoutubeAsset>();
             var atAssetList = new Dictionary<string, YoutubeAsset>();
 
@@ -145,6 +146,53 @@ namespace TakeDownAsset
                     }
                 }
             }
+
+            //using (var stream = new FileStream(atPath, FileMode.Create))
+            //{
+            //    using (var writer = new StreamWriter(stream, Encoding.UTF8))
+            //    {
+            //        using (var csvWriter = new CsvHelper.CsvWriter(writer, System.Threading.Thread.CurrentThread.CurrentCulture))
+            //        {
+            //            csvWriter.Configuration.HasHeaderRecord = true;
+            //            csvWriter.WriteField<string>("ddex_party_id");
+            //            csvWriter.WriteField<string>("album_artist");
+            //            csvWriter.WriteField<string>("album_artist_isnis");
+            //            csvWriter.WriteField<string>("album_title");
+            //            csvWriter.WriteField<string>("album_grid");
+            //            csvWriter.WriteField<string>("album_ean");
+            //            csvWriter.WriteField<string>("album_upc");
+            //            csvWriter.WriteField<string>("album_release_date");
+            //            csvWriter.WriteField<string>("album_label");
+            //            csvWriter.WriteField<string>("album_art_filename");
+            //            csvWriter.WriteField<string>("track_number");
+            //            csvWriter.WriteField<string>("track_title");
+            //            csvWriter.WriteField<string>("track_filename");
+            //            csvWriter.WriteField<string>("track_custom_id");
+            //            csvWriter.WriteField<string>("track_artist");
+            //            csvWriter.WriteField<string>("track_artist_isnis");
+            //            csvWriter.WriteField<string>("track_genres");
+            //            csvWriter.WriteField<string>("track_isrc");
+            //            csvWriter.WriteField<string>("track_pline");
+            //            csvWriter.WriteField<string>("track_territory_start_dates");
+            //            csvWriter.WriteField<string>("track_explicit_lyrics");
+            //            csvWriter.WriteField<string>("track_add_at_asset_labels");
+            //            csvWriter.WriteField<string>("track_add_sr_asset_labels");
+            //            csvWriter.NextRecord();
+
+            //            foreach (var asset in assetList)
+            //            {
+            //                foreach (var reference in asset.Value.ActiveReferenceIDList)
+            //                {
+            //                    csvWriter.WriteField(reference);
+            //                    csvWriter.WriteField("deactivate");
+            //                    csvWriter.WriteField("");
+            //                    csvWriter.NextRecord();
+            //                }
+            //            }
+            //            csvWriter.Flush();
+            //        }
+            //    }
+            //}
         }
     }
 }
