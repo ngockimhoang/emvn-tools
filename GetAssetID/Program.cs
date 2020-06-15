4b01f741974428067d5a4b6ae4f9b2a0f3934758
Kim Hoang <kimyumiko@gmail.com>
2020-06-15 22:11:58 +0700

    no message

--- GetAssetID/Program.cs
+++ GetAssetID/Program.cs
@@ -26,30 +26,9 @@ namespace GetAssetID
     {
         static void Main(string[] args)
         {
-            var youtubeReportPath = @"C:\Users\kimhoang\Desktop\EMVN\emvn-cms-prod\asset_full_report_Audiomachine_L_v1-1.csv";
-            var lavilleReportPath = @"C:\Users\kimhoang\Desktop\EMVN\emvn-cms-prod\laville_removal.csv";
-            var outputPath = @"C:\Users\kimhoang\Desktop\EMVN\emvn-cms-prod\laville_yt_assets.csv";
-            var lavilleTracks = new List<LavilleTrack>();
-            var ytAssets = new List<YoutubeAsset>();
-            using (var streamReader = System.IO.File.OpenText(lavilleReportPath))
-            {
-                using (var reader = new CsvHelper.CsvReader(streamReader, System.Threading.Thread.CurrentThread.CurrentCulture))
-                {
-                    reader.Read();
-                    reader.ReadHeader();
-                    while (reader.Read())
-                    {
-                        var track = new LavilleTrack()
-                        {
-                            Catalog = reader.GetField<string>("Catalogue ID"),
-                            AlbumID = reader.GetField<string>("Album ID#"),
-                            TrackID = reader.GetField<string>("Track ID#"),
-                        };
-                        lavilleTracks.Add(track);
-                    }
-                }
-
-            }
+            var youtubeReportPath = @"C:\Users\kimhoang\Desktop\EMVN\asset_full_report_Audiomachine_L_v1-1.csv";            
+            var outputPath = @"C:\Users\kimhoang\Desktop\EMVN\sr-delete.csv";            
+            var ytAssets = new List<YoutubeAsset>();           
 
             using (var streamReader = System.IO.File.OpenText(youtubeReportPath))
             {
@@ -59,35 +38,21 @@ namespace GetAssetID
                     reader.ReadHeader();
                     while (reader.Read())
                     {
-
                         if (reader.GetField<string>("asset_type") == "SOUND_RECORDING")
                         {
-                            var customID = reader.GetField<string>("custom_id").Trim().ToLower();
-                            var title = reader.GetField<string>("asset_title").Trim().ToLower();
-                            if (customID.StartsWith("apl"))
+                            var label = reader.GetField<string>("asset_label").Trim();
+                            if (label == "Minim"
+                                || label == "Lovely Music Library"
+                                || label == "Gothic Storm's Toolworks"
+                                || label == "Sham Stalin"
+                                || label == "WeWe"
+                                || label == "Shin Hong Vinh"
+                                || label == "Cuong Seven")
                             {
-                                if (lavilleTracks.Where(p => p.CustomID == customID).Any())
-                                {                                    
-                                    var ytAsset = new YoutubeAsset()
-                                    {
-                                        AssetID = reader.GetField<string>("asset_id"),
-                                        AssetTitle = reader.GetField<string>("asset_title")
-                                    };
-                                    ytAssets.Add(ytAsset);
-                                }
-
-                            }
-                            else if (title.StartsWith("apl"))
-                            {
-                                if (lavilleTracks.Where(p => p.CustomID == title).Any())
+                                ytAssets.Add(new YoutubeAsset()
                                 {
-                                    var ytAsset = new YoutubeAsset()
-                                    {
-                                        AssetID = reader.GetField<string>("asset_id"),
-                                        AssetTitle = reader.GetField<string>("asset_title")
-                                    };
-                                    ytAssets.Add(ytAsset);
-                                }
+                                    AssetID = reader.GetField<string>("asset_id").Trim()
+                                });
                             }
                         }
                     }
@@ -101,14 +66,12 @@ namespace GetAssetID
                     using (var csvWriter = new CsvHelper.CsvWriter(writer, System.Threading.Thread.CurrentThread.CurrentCulture))
                     {
                         csvWriter.Configuration.HasHeaderRecord = true;
-                        csvWriter.WriteField<string>("Asset_ID");
-                        csvWriter.WriteField<string>("Song_Title");
+                        csvWriter.WriteField<string>("Asset_ID");                        
                         csvWriter.NextRecord();
 
                         foreach (var asset in ytAssets)
                         {
                             csvWriter.WriteField<string>(asset.AssetID);
-                            csvWriter.WriteField<string>(asset.AssetTitle);
                             csvWriter.NextRecord();
                         }
                         csvWriter.Flush();
